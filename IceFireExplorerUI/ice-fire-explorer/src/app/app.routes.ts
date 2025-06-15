5b5d72d5910611b0e5a327d0a97c7bf389a42f8a
PC <danieldimov33@gmail.com>
2025-06-16 00:28:46 +0300

    Refactor: move routing configuration from app-routing.module.ts to app.routes.ts

--- IceFireExplorerUI/ice-fire-explorer/src/app/app.routes.ts
+++ IceFireExplorerUI/ice-fire-explorer/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HousesListComponent } from './pages/houses-list/houses-list.component';
+import { HouseDetailComponent } from './pages/house-detail/house-detail.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'houses', component: HousesListComponent },
+    { path: 'houses/:id', component: HouseDetailComponent },
+    { path: 'favorites', component: FavoritesComponent },
+  ];
+  
